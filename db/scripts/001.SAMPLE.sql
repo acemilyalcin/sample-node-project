708af0c6ab7bf76fa7541075cad2f4abe5bc8b70
Chathuranga bandara <72471769+chathunb@users.noreply.github.com>
2022-01-22 08:37:54 +0530

    Update 001.SAMPLE.sql

--- db/scripts/001.SAMPLE.sql
+++ db/scripts/001.SAMPLE.sql
@@ -1,12 +1,4 @@
---liquibase formatted sql
+-- liquibase formatted sql
 
---changeset salerno:3
-CREATE TABLE accounts_test_chathun (
-	user_id serial PRIMARY KEY,
-	username VARCHAR ( 50 ) UNIQUE NOT NULL,
-	password VARCHAR ( 50 ) NOT NULL,
-	email VARCHAR ( 255 ) UNIQUE NOT NULL,
-	created_on TIMESTAMP NOT NULL,
-        last_login TIMESTAMP 
-);
---rollback drop table test1;
+-- changeset liquibase:1
+CREATE TABLE test_table (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
