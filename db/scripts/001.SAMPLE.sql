aee6d2a174dbe3ac66bf4029d754820e9a71be7d
Chathuranga bandara <72471769+chathunb@users.noreply.github.com>
2022-01-24 10:21:38 +0530

    Update 001.SAMPLE.sql

--- db/scripts/001.SAMPLE.sql
+++ db/scripts/001.SAMPLE.sql
@@ -1,2 +1,2 @@
 -- changeset liquibase:1
-CREATE TABLE test_table (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
+CREATE TABLE `account`.test_table (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
