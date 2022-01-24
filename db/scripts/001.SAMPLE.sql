644ed8e2cc145fb2de3d2b788d7110abba2e0cce
Chathuranga bandara <72471769+chathunb@users.noreply.github.com>
2022-01-24 10:49:29 +0530

    Update 001.SAMPLE.sql

--- db/scripts/001.SAMPLE.sql
+++ db/scripts/001.SAMPLE.sql
@@ -1,14 +1,4 @@
 -- liquibase formatted sql
 
 -- changeset liquibase:1
-CREATE TABLE account.test_table1 (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
-
-create table account."user_customer_test" (
-    "user_customer_id" serial PRIMARY KEY,
-    "user_id" varchar(255) not null,
-    "customer_id" varchar(255) not null,
-    "created_by" varchar(255) not null,
-    "updated_by" varchar(255) not null,
-    "created_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE account.test_table2 (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
