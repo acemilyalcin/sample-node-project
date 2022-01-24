69251ca3434e928d9c395463625fc6bbb31c4597
Chathuranga bandara <72471769+chathunb@users.noreply.github.com>
2022-01-24 09:50:03 +0530

    Update 001.SAMPLE.sql

--- db/scripts/001.SAMPLE.sql
+++ db/scripts/001.SAMPLE.sql
@@ -1,4 +1,4 @@
 -- liquibase formatted sql
 
 -- changeset liquibase:1
-CREATE TABLE order.test_table (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
+CREATE TABLE account.test_table (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
