f0bfdd386a970ddb01d487d90860643a4a7cf416
Chathuranga bandara <72471769+chathunb@users.noreply.github.com>
2022-01-24 10:42:53 +0530

    Update 001.SAMPLE.sql

--- db/scripts/001.SAMPLE.sql
+++ db/scripts/001.SAMPLE.sql
@@ -1,3 +1,8 @@
+-- liquibase formatted sql
+
+-- changeset liquibase:1
+CREATE TABLE account.test_table1 (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id))
+
 create table account."user_customer_test" (
     "user_customer_id" serial PRIMARY KEY,
     "user_id" varchar(255) not null,
