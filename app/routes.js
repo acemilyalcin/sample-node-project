03a31412805127e0a0f1be50484bd2fdba4a1236
acyalcin <acemilyalcin@gmail.com>
2025-07-06 03:55:16 +0200

    Improved the comments

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,5 @@
 module.exports = function(app) {
-
-    app.get('/',function(req,res){ // İSTEMCİNİN '/' İSTEĞİNE KARŞILIK VEREN KOD BLOĞU
-        res.render('index'); // INDEX VİEW DOSYASI RENDER EDİLDİ
+    app.get('/', function(req, res){
+        res.render('index'); // index view file is rendered when HTTP GET '/' called
     });
-
 }
