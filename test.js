ee4424bfad432d0a01efeeef8d7335322e172fd4
Gaurav <gauravkumarbth@gmail.com>
2025-09-25 18:19:10 +0530

    changes

--- test.js
+++ test.js
@@ -5,7 +5,6 @@ const request = supertest(app);
 app.get("/", function (req, res) {
     res.status(200).send("practise with kubernetes");
 });
-
 describe("GET /", function () {
     it("should return 200 OK", function (done) {
         request
